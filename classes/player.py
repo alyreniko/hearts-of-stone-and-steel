0e9d501beef780dfd094968f2042bea575891186
dyrhyrv <dyrhyrv@ya.ru>
2024-12-02 00:19:19 +0700

    Добавлена анимация ходьбы и бездействия

--- classes/player.py
+++ classes/player.py
@@ -7,35 +7,48 @@ from classes.interaction import Interaction
 class Player:
     def __init__(self):
         self.path = "assets/sprites/player/"
-        self.images = [f"{self.path}{i}.png" for i in range(1, 8)]
-        self.index = 0
+        self.walk_images = [f"{self.path}/walk/{i}.png" for i in range(1, 8)]
+        self.idle_images = [f"{self.path}/idle/{i}.png" for i in range(1, 20)]
+        self.walk_index = 0
+        self.idle_index = 0
         self.image = pygame.transform.scale(
-            pygame.image.load(self.images[self.index]).convert_alpha(), (256, 256)
+            pygame.image.load(self.idle_images[self.idle_index]).convert_alpha(), (256, 256)
         )
         self.rect = self.image.get_rect()
         self.rect.center = (100, 920)
         self.speed = 10
         self.direction = "right"
+        self.state = "idle"
         self.original_image = self.image
         self.frame_count = 0
 
     def update(self, bg_rect):
         if self.frame_count % 4 == 0:
-            self.index = (self.index + 1) % len(self.images)
-            self.image = pygame.transform.scale(
-                pygame.image.load(self.images[self.index]).convert_alpha(), (256, 256)
-            )
+            if self.state == "idle":
+                self.image = pygame.transform.scale(
+                    pygame.image.load(self.idle_images[self.idle_index]).convert_alpha(), (256, 256)
+                )
+                self.idle_index = (self.idle_index + 1) % len(self.idle_images)
+            elif self.state == "walk":
+                self.image = pygame.transform.scale(
+                    pygame.image.load(self.walk_images[self.walk_index]).convert_alpha(), (256, 256)
+                )
+                self.walk_index = (self.walk_index + 1) % len(self.walk_images)
             if Control.keys[var.MOVE_LEFT]:
-                if self.direction != "left":
-                    self.direction = "left"
+                self.state = "walk"
+                self.direction = "left"
                 self.image = pygame.transform.flip(self.image, True, False)
             elif Control.keys[var.MOVE_RIGHT]:
-                if self.direction != "right":
-                    self.direction = "right"
-                self.image = pygame.transform.flip(self.image, False, False)
+                self.state = "walk"
+                self.direction = "right"
             else:
-                self.direction = "idle"
-                self.image = self.original_image
+                self.state = "idle"
+                if self.direction == "left":
+                    self.image = pygame.transform.flip(self.image, True, False)
+                    # self.direction = "right"
+                elif self.direction == "right":
+                    self.image = pygame.transform.flip(self.image, False, False)
+                    # self.direction = "left"
             self.frame_count = 0
 
         self.frame_count += 1
@@ -57,4 +70,3 @@ class Player:
 
     def reset_position(self, x, y):
         self.rect.center = (x, y)
-
